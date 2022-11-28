7221515792dc1f17911ede715dcafa7d1be17438
unknown <you@example.com>
2022-11-28 17:30:19 +0800

    first commit

--- _nuxt/index.a0c65a6c.js
+++
@@ -1,1 +0,0 @@
-import{a as e,e as n,o}from"./entry.806d36a6.js";const t={};function a(c,r,s,p,_,f){return o(),n("main",null," \u9996\u9875 ")}const l=e(t,[["render",a]]);export{l as default};
---
+++ _nuxt/index.fc4aaded.js
@@ -0,0 +1,1 @@
+import{a as e,e as n,o}from"./entry.6445d97b.js";const t={};function a(c,r,s,p,_,f){return o(),n("main",null," \u9996\u9875 ")}const l=e(t,[["render",a]]);export{l as default};
